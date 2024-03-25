08267a3f82bf3a4f80c7980a9a1593121e5b353d
gldwolf <zengqiangzhao@outlook.com>
2024-03-25 22:21:49 +0800

    modify some comments

--- kernel/chr_drv/tty_io.c
+++ kernel/chr_drv/tty_io.c
@@ -11,7 +11,7 @@
  * Kill-line thanks to John T Kohl, who also corrected VMIN = VTIME = 0.
  */
 /*
- * 'tty_io.c' 给 tty 终端一种非相关的感觉, 不管它们是控制台还是品串行终端.
+ * 'tty_io.c' 给 tty 终端一种非相关的感觉, 不管它们是控制台还是串行终端.
  * 该程序同样实现了回显, 规范(熟)模式等.
  */
 
@@ -29,63 +29,63 @@
 #include <asm/system.h>									// 系统头文件. 定义设置或修改描述符/中断门等嵌入式汇编宏.
 
 int kill_pg(int pgrp, int sig, int priv);
-int is_orphaned_pgrp(int pgrp);                 		// 判断是否孤儿进程
+int is_orphaned_pgrp(int pgrp);                 		// 判断是否孤儿进程.
 
 // 获取 termios 结构中三个模式标志集之一, 或者用于判断一个标志集是否有置位标志.
-#define _L_FLAG(tty, f)	((tty)->termios.c_lflag & f)	// 本地模式标志
-#define _I_FLAG(tty, f)	((tty)->termios.c_iflag & f)	// 输入模式标志
-#define _O_FLAG(tty, f)	((tty)->termios.c_oflag & f)	// 输出模式标志
+#define _L_FLAG(tty, f)	((tty)->termios.c_lflag & f)	// 本地模式标志.
+#define _I_FLAG(tty, f)	((tty)->termios.c_iflag & f)	// 输入模式标志.
+#define _O_FLAG(tty, f)	((tty)->termios.c_oflag & f)	// 输出模式标志.
 
 // 取 termios 结构终端特殊(本地)模式标志集中的一个标志.
 #define L_CANON(tty)	_L_FLAG((tty), ICANON)			// 取规范模式标志.
-#define L_ISIG(tty)		_L_FLAG((tty), ISIG)			// 取信号标志
-#define L_ECHO(tty)		_L_FLAG((tty), ECHO)			// 取回显字符标志
+#define L_ISIG(tty)		_L_FLAG((tty), ISIG)			// 取信号标志.
+#define L_ECHO(tty)		_L_FLAG((tty), ECHO)			// 取回显字符标志.
 #define L_ECHOE(tty)	_L_FLAG((tty), ECHOE)			// 规范模式时取回显擦除标志.
-#define L_ECHOK(tty)	_L_FLAG((tty), ECHOK)			// 规范模式时取 KILL 擦除当前行标志
-#define L_ECHOCTL(tty)	_L_FLAG((tty), ECHOCTL)			// 取回显控制字符标志
-#define L_ECHOKE(tty)	_L_FLAG((tty), ECHOKE)			// 规范模式时取 KILL 擦除行并回显标志
-#define L_TOSTOP(tty)	_L_FLAG((tty), TOSTOP)			// 对于后台输出发送 SIGTTOU 信号
+#define L_ECHOK(tty)	_L_FLAG((tty), ECHOK)			// 规范模式时取 KILL 擦除当前行标志.
+#define L_ECHOCTL(tty)	_L_FLAG((tty), ECHOCTL)			// 取回显控制字符标志.
+#define L_ECHOKE(tty)	_L_FLAG((tty), ECHOKE)			// 规范模式时取 KILL 擦除行并回显标志.
+#define L_TOSTOP(tty)	_L_FLAG((tty), TOSTOP)			// 对于后台输出发送 SIGTTOU 信号.
 
-// 取 termios 结构输入模式标志集中的一个标志
-#define I_UCLC(tty)		_I_FLAG((tty), IUCLC)			// 取大写到小写转换标志
-#define I_NLCR(tty)		_I_FLAG((tty), INLCR)			// 取换行符 NL 转回车符 CR 标志
-#define I_CRNL(tty)		_I_FLAG((tty), ICRNL)			// 取回车符 CR 转换行符 NL 标志
-#define I_NOCR(tty)		_I_FLAG((tty), IGNCR)			// 取忽略回车符 CR 标志
-#define I_IXON(tty)		_I_FLAG((tty), IXON)			// 取输入控制流标志 XON
+// 取 termios 结构输入模式标志集中的一个标志.
+#define I_UCLC(tty)		_I_FLAG((tty), IUCLC)			// 取大写到小写转换标志.
+#define I_NLCR(tty)		_I_FLAG((tty), INLCR)			// 取换行符 NL 转回车符 CR 标志.
+#define I_CRNL(tty)		_I_FLAG((tty), ICRNL)			// 取回车符 CR 转换行符 NL 标志.
+#define I_NOCR(tty)		_I_FLAG((tty), IGNCR)			// 取忽略回车符 CR 标志.
+#define I_IXON(tty)		_I_FLAG((tty), IXON)			// 取输入控制流标志 XON.
 
 // 取 termios 结构输出模式标志集中的一个标志.
-#define O_POST(tty)		_O_FLAG((tty), OPOST)			// 取执行输出处理标志
-#define O_NLCR(tty)		_O_FLAG((tty), ONLCR)			// 取换行符 NL 转回车换行符 CR-NL 标志
-#define O_CRNL(tty)		_O_FLAG((tty), OCRNL)			// 取回车符 CR 转换行符 NL 标志
-#define O_NLRET(tty)	_O_FLAG((tty), ONLRET)			// 取换行符 NL 执行回车功能的标志
-#define O_LCUC(tty)		_O_FLAG((tty), OLCUC)			// 取小写转大写字符标志
+#define O_POST(tty)		_O_FLAG((tty), OPOST)			// 取执行输出处理标志.
+#define O_NLCR(tty)		_O_FLAG((tty), ONLCR)			// 取换行符 NL 转回车换行符 CR-NL 标志.
+#define O_CRNL(tty)		_O_FLAG((tty), OCRNL)			// 取回车符 CR 转换行符 NL 标志.
+#define O_NLRET(tty)	_O_FLAG((tty), ONLRET)			// 取换行符 NL 执行回车功能的标志.
+#define O_LCUC(tty)		_O_FLAG((tty), OLCUC)			// 取小写转大写字符标志.
 
 // 取 termios 结构控制标志集中波特率. CBAUD 是波特率屏蔽码(0000017). 
 #define C_SPEED(tty)	((tty)->termios.c_cflag & CBAUD)
 // 判断 tty 终端是否已挂线(hang up), 即其传输波特率是否为 B0(0). 
 #define C_HUP(tty)	(C_SPEED((tty)) == B0)
 
-// 取最小值宏
+// 取最小值宏.
 #ifndef MIN
 #define MIN(a,b) ((a) < (b) ? (a) : (b))
 #endif
 
-// tty_struct - tty 终端, tty_queue - tty 缓冲队列
+// tty_struct - tty 终端, tty_queue - tty 缓冲队列.
 // 下面定义 tty 终端使用的缓冲队列结构数组 tty_queues 和 tty 终端表结构数组 tty_table.
 // QUEUES 是 tty 终端使用的缓冲队列最大数量. 伪终端分主从两种(master 和 slave). 
 // 每个 tty 终端(tty_struct)使用 3 个 tty 缓冲队列(tty_queue), 它们分别是:
-// read_queue: 		用于缓冲键盘或串行输入的读队列
-// write_queue: 	用于缓冲屏幕或串行输出的写队列
-// secondary: 		用于保存规范模式字符的辅助缓冲队列
+// read_queue: 		用于缓冲键盘或串行输入的读队列.
+// write_queue: 	用于缓冲屏幕或串行输出的写队列.
+// secondary: 		用于保存规范模式字符的辅助缓冲队列.
 #define QUEUES	(3 * (MAX_CONSOLES + NR_SERIALS + 2 * NR_PTYS))		// 共 54 项(3 * (8 + 2 + 2 * 4) = 3 * 18 = 54).
-static struct tty_queue tty_queues[QUEUES];							// tty 缓冲队列数组.
-struct tty_struct tty_table[256];									// tty 表结构数组. 用于保存每个终端设备的信息，每项对应系统的一个终端设备
+static struct tty_queue tty_queues[QUEUES];			// tty 缓冲队列数组.
+struct tty_struct tty_table[256];					// tty 表结构数组. 用于保存每个终端设备的信息，每项对应系统的一个终端设备.
 
 // 下面设定各种类型的 tty 终端所使用缓冲队列结构在 tty_queues[] 数组中的起始项位置.
-// 8 个虚拟控制台终端占用 tty_queues[] 数组开头 24 项(3 * MAX_CONSOLES)(0 -- 23)
-// 2 个串行终端(rs_queues)占用随后的 6 项(3 * NR_SERIALS)(24 -- 29)
-// 4 个主伪终端(mpty_queues)占用随后的 12项(3 * NR_PTYS)(30 -- 41)
-// 4 个从伪终端(spty_queues)占用随后的 12 项(3 * NR_PTYS)(42 -- 53)
+// 8 个虚拟控制台终端占用 tty_queues[] 数组开头 24 项(3 * MAX_CONSOLES)(0 -- 23).
+// 2 个串行终端(rs_queues)占用随后的 6 项(3 * NR_SERIALS)(24 -- 29).
+// 4 个主伪终端(mpty_queues)占用随后的 12 项(3 * NR_PTYS)(30 -- 41).
+// 4 个从伪终端(spty_queues)占用随后的 12 项(3 * NR_PTYS)(42 -- 53).
 #define con_queues tty_queues
 #define rs_queues ((3 * MAX_CONSOLES) + tty_queues)
 #define mpty_queues ((3 * (MAX_CONSOLES + NR_SERIALS)) + tty_queues)
@@ -108,18 +108,18 @@ int fg_console = 0;									// 当前前台控制台号(范围 0--7).
  * you can implement virtual consoles.
  */
 /*
- * 下面是汇编程序中使用的缓冲队列结构地址表. 通过修改这个表, 你可以实现虚拟控制台
+ * 下面是汇编程序中使用的缓冲队列结构地址表. 通过修改这个表, 你可以实现虚拟控制台.
  */
 // tty 读写缓冲队列结构地址表. 供 rs_io.s 程序使用, 用于取得读写缓冲队列结构的地址.
 struct tty_queue * table_list[] = {
-	con_queues + 0, con_queues + 1,					// 前台控制台读, 写队列结构地址
-	rs_queues + 0, rs_queues + 1,					// 串行终端 1 读, 写队列结构地址
-	rs_queues + 3, rs_queues + 4					// 串行终端 2 读, 写队列结构地址
+	con_queues + 0, con_queues + 1,					// 前台控制台读, 写队列结构地址.
+	rs_queues + 0, rs_queues + 1,					// 串行终端 1 读, 写队列结构地址.
+	rs_queues + 3, rs_queues + 4					// 串行终端 2 读, 写队列结构地址.
 };
 
 // 改变前台控制台.
-// 将前台控制台设定为指定的虚拟控制台
-// 参数: new_console - 指定的新控制台号
+// 将前台控制台设定为指定的虚拟控制台.
+// 参数: new_console - 指定的新控制台号.
 void change_console(unsigned int new_console)
 {
 	// 如果参数指定的控制台已经在前台或者参数无效, 则退出. 否则设置当前前台控制台号, 
@@ -167,7 +167,8 @@ void wait_for_keypress(void)
 }
 
 // 复制成规范模式字符序列
-// 根据终端 termios 结构中设置的各种标志, 将指定 tty 同读队列缓冲区中的字符复制转换成规范模式(熟模式)字符并存放在辅助队列(规范模式队列)中.
+// 根据终端 termios 结构中设置的各种标志, 
+// 将指定 tty 同读队列缓冲区中的字符复制转换成规范模式(熟模式)字符并存放在辅助队列(规范模式队列)中.
 // 参数: tty - 指定终端的 tty 结构指针.
 void copy_to_cooked(struct tty_struct * tty)
 {
@@ -178,20 +179,22 @@ void copy_to_cooked(struct tty_struct * tty)
 		printk("copy_to_cooked: missing queues\n\r");
 		return;
 	}
-	// 否则我们根据终端 termios 结构中的输入和本地标志, 对从 tty 读队列缓冲区中取出的每个字符进行适当的处理, 
-	// 然后放入辅助队列 secondary 中. 在下面循环体中, 如果此时读队列缓冲区已经取空或都辅助队列缓冲区已经放满字符, 就退出循环体. 
+	// 否则我们根据终端 termios 结构中的输入和本地标志, 
+	// 对从 tty 读队列缓冲区中取出的每个字符进行适当的处理, 然后放入辅助队列 secondary 中. 
+	// 在下面循环体中, 如果此时读队列缓冲区已经取空或都辅助队列缓冲区已经放满字符, 就退出循环体. 
 	// 否则程序就从读队列缓冲区尾指针处取一字符, 并把尾指针前移一个字符位置. 然后根据该字符代码值进行处理.
-	// 另外, 如果定义了 _POSIX_VDISABLE(\0), 那么在对字符处理过程中, 若字符代码值等于 _POSIX_VDISABLE 的值时, 表示禁止使用相应特殊控制字符的功能.
+	// 另外, 如果定义了 _POSIX_VDISABLE(\0), 那么在对字符处理过程中, 
+	// 若字符代码值等于 _POSIX_VDISABLE 的值时, 表示禁止使用相应特殊控制字符的功能.
 	while (1) {
-		// 如果 tty 对应的读队列为空则直接立刻中断循环
+		// 如果 tty 对应的读队列为空则直接立刻中断循环.
 		if (EMPTY(tty->read_q))
 			break;
-		// 如果 tty 对应的第三个队列为空则直接立刻中断循环
+		// 如果 tty 对应的第三个队列为空则直接立刻中断循环.
 		if (FULL(tty->secondary))
 			break;
-		GETCH(tty->read_q, c);								// 取一字符到 c, 并前移尾指针
+		GETCH(tty->read_q, c);								// 取一字符到 c, 并前移尾指针.
 		// 如果该字符是回车符 CR(13), 那么若回车换行转换行 CRNL 置位, 则将字符转换为换行符 NL(10). 
-		// 否则如果忽略回车标志 NOCR 置位, 则忽略该字符, 继续处理其他字符
+		// 否则如果忽略回车标志 NOCR 置位, 则忽略该字符, 继续处理其他字符.
 		if (c == 13) {
 			if (I_CRNL(tty))
 				c = 10;
@@ -219,8 +222,8 @@ void copy_to_cooked(struct tty_struct * tty)
 				while(!(EMPTY(tty->secondary) ||
 				        (c = LAST(tty->secondary)) == 10 ||
 				        ((EOF_CHAR(tty) != _POSIX_VDISABLE) && (c == EOF_CHAR(tty))))) {
-					if (L_ECHO(tty)) {						// 若本地回显标志置位
-						if (c < 32)							// 控制字符要删 2 字节
+					if (L_ECHO(tty)) {						// 若本地回显标志置位.
+						if (c < 32)							// 控制字符要删 2 字节.
 							PUTCH(127, tty->write_q);
 						PUTCH(127, tty->write_q);
 						tty->write(tty);
@@ -250,9 +253,12 @@ void copy_to_cooked(struct tty_struct * tty)
 				continue;
 			}
         }
-		// 如果设置了 IXON 标志, 则使终端停止/开始输出控制字符起作用. 如果没有设置此标志, 那么停止和开始字符将被作为一般字符供进程读取. 
-		// 在这段代码中, 如果读取的字符是停止字符 STOP(^S), 则置 tty 停止标志, 让 tty 暂停输出. 同时丢弃该特殊控制字符(不放入辅助队列中),
-		// 并继续处理其他字符. 如果字符是开始字符 START(^Q), 则复位 tty 停止标志, 恢复 tty 输出. 同时丢弃该控制字符, 并继续处理其他字符. 
+		// 如果设置了 IXON 标志, 则使终端停止/开始输出控制字符起作用. 
+		// 如果没有设置此标志, 那么停止和开始字符将被作为一般字符供进程读取. 
+		// 在这段代码中, 如果读取的字符是停止字符 STOP(^S), 则置 tty 停止标志, 让 tty 暂停输出. 
+		// 同时丢弃该特殊控制字符(不放入辅助队列中), 并继续处理其他字符. 
+		// 如果字符是开始字符 START(^Q), 则复位 tty 停止标志, 恢复 tty 输出. 
+		// 同时丢弃该控制字符, 并继续处理其他字符. 
 		// 对于控制台来说, 这里的 tty->write() 是 console.c 中的 con_write() 函数. 
 		// 因此对于伪终端也是由于设置了终端 stopped 标志而会暂停写操作(chr_drv/console.c). 
 		// 对于伪终端也是由于设置了终端 stopped 标志而会暂停写操作(chr_drv/pty.c).
@@ -269,10 +275,12 @@ void copy_to_cooked(struct tty_struct * tty)
 				continue;
 			}
         }
-		// 若输入模式标志集中 ISIG 标志置位, 表示终端键盘可以产生信号, 则在收到控制字符 INTR, QUIT, SUSP 或 DSUSP 时, 
-		// 需要为进程产生相应的信号. 如果该字符是键盘中断符(^C), 则向当前进程之进程组中所有进程发送键盘中断信号 SIGINT, 并继续处理下一字符. 
-		// 如果该字符是退出符(^\), 则向当前进程之进程组中所有进程发送键盘退出信号 SIGQUIT, 并继续处理下一字符. 如果字符是暂停符(^Z), 
-		// 则向当前进程发送暂停信息 SIGTSTP. 同样, 若定义了 _POSIX_VDISABLE(\0), 那么在对字符处理过程中, 
+		// 若输入模式标志集中 ISIG 标志置位, 表示终端键盘可以产生信号, 
+		// 则在收到控制字符 INTR, QUIT, SUSP 或 DSUSP 时, 需要为进程产生相应的信号. 
+		// 如果该字符是键盘中断符(^C), 则向当前进程之进程组中所有进程发送键盘中断信号 SIGINT, 并继续处理下一字符. 
+		// 如果该字符是退出符(^\), 则向当前进程之进程组中所有进程发送键盘退出信号 SIGQUIT, 并继续处理下一字符. 
+		// 如果字符是暂停符(^Z), 则向当前进程发送暂停信息 SIGTSTP. 
+		// 同样, 若定义了 _POSIX_VDISABLE(\0), 那么在对字符处理过程中, 
 		// 若字符代码值等于 _POSIX_VDISABLE 的值时, 表示禁止使用相应特殊控制字符的功能.
 		if (L_ISIG(tty)) {
 			if ((INTR_CHAR(tty) != _POSIX_VDISABLE) && (c==INTR_CHAR(tty))) {
@@ -284,17 +292,20 @@ void copy_to_cooked(struct tty_struct * tty)
 				continue;
 			}
 			if ((SUSPEND_CHAR(tty) != _POSIX_VDISABLE) && (c == SUSPEND_CHAR(tty))) {
-				if (!is_orphaned_pgrp(tty->pgrp))				// 判断一个进程组是否孤儿进程
+				if (!is_orphaned_pgrp(tty->pgrp))				// 判断一个进程组是否孤儿进程.
 					kill_pg(tty->pgrp, SIGTSTP, 1);
 				continue;
 			}
 		}
-		// 如果该字符是换行符 NL(10), 或者是文件结束符 EOF(4,^D), 表示一行字符已处理完, 则把辅助缓冲队列中当前含有字符行数值 secondar. 
+		// 如果该字符是换行符 NL(10), 或者是文件结束符 EOF(4,^D), 
+		// 表示一行字符已处理完, 则把辅助缓冲队列中当前含有字符行数值 secondar. 
 		// data 增 1. 如果在函数 tty_read() 中取走一行字符, 该值即会减 1.
 		if (c == 10 || (EOF_CHAR(tty) != _POSIX_VDISABLE && c == EOF_CHAR(tty)))
 			tty->secondary->data++;
-		// 如果本地模式标志中回显标志 ECHO 在置位状态, 那么, 如果字符是换行符 NL(10), 则将换行符 NL(10) 和回车符(13) 放入 tty 写队列缓冲区中; 
-		// 如果字符是控制字符(值 < 32)并且回显控制字符标志 ECHOCTL 置位, 则将字符 '^' 和字符 c + 64 放入 tty 写队列中(也即会显示 ^C, ^H 等); 
+		// 如果本地模式标志中回显标志 ECHO 在置位状态, 那么, 如果字符是换行符 NL(10), 
+		// 则将换行符 NL(10) 和回车符(13) 放入 tty 写队列缓冲区中; 
+		// 如果字符是控制字符(值 < 32)并且回显控制字符标志 ECHOCTL 置位, 
+		// 则将字符 '^' 和字符 c + 64 放入 tty 写队列中(也即会显示 ^C, ^H 等); 
 		// 否则将该字符直接放入 tty 写缓冲队列中. 最后调用该 tty 写操作函数.
 		if (L_ECHO(tty)) {
 			if (c == 10) {
@@ -309,7 +320,7 @@ void copy_to_cooked(struct tty_struct * tty)
 				PUTCH(c, tty->write_q);
 			tty->write(tty);
 		}
-		// 每一次循环末将处理过的字符放入辅助队列中
+		// 每一次循环末将处理过的字符放入辅助队列中.
 		PUTCH(c, tty->secondary);
     }
 	// 在退出循环体后唤醒等待该辅助缓冲队列的进程(如果有的话).
@@ -373,15 +384,17 @@ int tty_read(unsigned channel, char * buf, int nr)
 	char c, *b = buf;
 	int minimum, time;
 
-	// 首先判断参数有效性并取终端的 tty 结构指针. 如果 tty 终端的三个缓冲队列指针都是 NULL, 则返回 EIO 出错信息. 
+	// 首先判断参数有效性并取终端的 tty 结构指针. 
+	// 如果 tty 终端的三个缓冲队列指针都是 NULL, 则返回 EIO 出错信息. 
 	// 如果 tty 终端是一个伪终端, 则再取得另一个对应伪终端的 tty 结构 other_tty. 
 	if (channel > 255)
 		return -EIO;
 	tty = TTY_TABLE(channel);
 	if (!(tty->write_q || tty->read_q || tty->secondary))
 		return -EIO;
-	// 如果当前进程使用的是这里正在处理的 tty 终端, 但该终端的进程组号却与当前进程组号不同, 表示当前进程是后台进程组中的一个进程, 即进程不在前台. 
-	// 于是我们要停止当前进程组的所有进程. 因此这里就需要向当前进程组发送 SIGTTIN 信号, 并返回等待成为前台进程组后再执行读操作. 
+	// 如果当前进程使用的是这里正在处理的 tty 终端, 但该终端的进程组号却与当前进程组号不同, 
+	// 表示当前进程是后台进程组中的一个进程, 即进程不在前台, 于是我们要停止当前进程组的所有进程. 
+	// 因此这里就需要向当前进程组发送 SIGTTIN 信号, 并返回等待成为前台进程组后再执行读操作. 
 	if ((current->tty == channel) && (tty->pgrp != current->pgrp))
 		return(tty_signal(SIGTTIN, tty));
 	// 如果当前终端是伪终端, 那么对应的另一个伪终端就是 other_tty. 
@@ -389,7 +402,8 @@ int tty_read(unsigned channel, char * buf, int nr)
 	if (channel & 0x80)
 		other_tty = tty_table + (channel ^ 0x40);
 	// 然后根据 VTIME 和 VMIN 对应的控制字符数组值设置读字符操作超时定时值 time 和最少需要读取的字符个数 minimum. 
-	// 在非规范模式下, 这两个是超时定时值. VMIN 表示为了满足读操作而需要读取的最少字符个数. VTIME 是一个 1/10 秒计数计时值. 
+	// 在非规范模式下, 这两个是超时定时值. 
+	// VMIN 表示为了满足读操作而需要读取的最少字符个数. VTIME 是一个 1/10 秒计数计时值. 
 	time = 10L * tty->termios.c_cc[VTIME];            				// 设置读操作超时定时值. 
 	minimum = tty->termios.c_cc[VMIN];              				// 最少需要读取的字符个数. 
 	// 如果 tty 终端处于规范模式, 则设置最小要读取字符数 minimum 等于进程欲读字符数 nr. 
@@ -445,9 +459,12 @@ int tty_read(unsigned channel, char * buf, int nr)
 		// 下面开始正式执行取字符操作. 需读字符数 nr 依次递减, 直到 nr = 0或者辅助缓冲队列为空. 
 		// 在这个循环过程中, 首先取辅助缓冲队列字符 c, 并且把缓冲队列尾指针 tail 向右移动一个字符位置. 
 		// 如果所取字符是文件结束符(^D)或者是换行符 NL(10), 则把辅助缓冲队列中含有字符行数值减 1. 
-		// 如果该字符是文件结束符(^D)并且规范模式标志成置位状态, 则中断本循环, 否则说明现在还没有遇到文件结束符或者正处于原始(非规范)模式. 
-		// 在这种模式中用户以字符流作为读取对象, 也不识别其中的控制字符(如文件结束符). 于是将字符直接放入用户数据缓冲区 buf 中, 并把欲读字符数减 1.
-		// 此时如果欲读字符数已为 0 则中断循环. 另外, 如果终端处于规范模式并且读取的字符是换行符 NL(10), 则也退出循环. 
+		// 如果该字符是文件结束符(^D)并且规范模式标志成置位状态, 则中断本循环, 
+		// 否则说明现在还没有遇到文件结束符或者正处于原始(非规范)模式. 
+		// 在这种模式中用户以字符流作为读取对象, 也不识别其中的控制字符(如文件结束符). 
+		// 于是将字符直接放入用户数据缓冲区 buf 中, 并把欲读字符数减 1.
+		// 此时如果欲读字符数已为 0 则中断循环. 
+		// 另外, 如果终端处于规范模式并且读取的字符是换行符 NL(10), 则也退出循环. 
 		// 除此之外, 只要还没有取完欲读字符数 nr 并且辅助队列不为空, 就继续取队列中的字符. 
 		do {
 			GETCH(tty->secondary, c);
@@ -463,11 +480,12 @@ int tty_read(unsigned channel, char * buf, int nr)
 			if (c == 10 && L_CANON(tty))
 				break;
 		} while (nr > 0 && !EMPTY(tty->secondary));
-		// 执行到此, 那么如果 tty 终端处于规范模式下, 说明我们可能读到了换行符或者遇到了文件结束符. 如果是处于非规范模式下, 
-		// 那么说明我们已经读取了 nr 个字符, 或者辅助队列已经被取空了. 于是我们首先唤醒等待队列的进程, 然后看看是否设置过超时定时值 time. 
+		// 执行到此, 那么如果 tty 终端处于规范模式下, 说明我们可能读到了换行符或者遇到了文件结束符. 
+		// 如果是处于非规范模式下, 那么说明我们已经读取了 nr 个字符, 或者辅助队列已经被取空了. 
+		// 于是我们首先唤醒等待队列的进程, 然后看看是否设置过超时定时值 time. 
 		// 如果超时定时值 time 不为 0, 我们就要求等待一定的时间让其他进程可以把字符写入读队列中. 
-		// 于是设置进程读超时定时值为系统当前时间 jiffies + 读超时值 time. 当然, 如果终端处于规范模式, 或者已经读取 nr 个字符, 
-		// 我们就可以直接退出这个大循环了. 
+		// 于是设置进程读超时定时值为系统当前时间 jiffies + 读超时值 time. 
+		// 当然, 如果终端处于规范模式, 或者已经读取 nr 个字符, 我们就可以直接退出这个大循环了. 
 		wake_up(&tty->read_q->proc_list);
 		if (time)
 			current->timeout = time + jiffies;
@@ -493,20 +511,24 @@ int tty_write(unsigned channel, char * buf, int nr)
 	struct tty_struct * tty;
 	char c, *b = buf;
 
-	// 首先判断参数有效性并取终端的 tty 结构指针. 如果 tty 终端的三个缓冲队列指针都是 NULL, 则返回 EIO 出错信息.
+	// 首先判断参数有效性并取终端的 tty 结构指针. 
+	// 如果 tty 终端的三个缓冲队列指针都是 NULL, 则返回 EIO 出错信息.
 	if (channel > 255)
 		return -EIO;
 	tty = TTY_TABLE(channel);
 	if (!(tty->write_q || tty->read_q || tty->secondary))
 		return -EIO;
-	// 如果若终端本地模式标志集中设置了 TOSTOP, 表示后台进程输出时需要发送信号 SIGTTOU. 如果当前进程使用的是这里正在处理的 tty 终端, 
-	// 但该终端的进程组号却与当前进程组号不同, 即表示当前进程是后台进程组中的一个进程, 即进程不在前台. 
-	// 于是我们要停止当前进程组的所有进程. 因此这里就需要向当前进程组发送 SIGTTOU 信号, 并返回等待成为前台进程组后再执行写操作.
+	// 如果若终端本地模式标志集中设置了 TOSTOP, 表示后台进程输出时需要发送信号 SIGTTOU. 
+	// 如果当前进程使用的是这里正在处理的 tty 终端, 但该终端的进程组号却与当前进程组号不同, 
+	// 即表示当前进程是后台进程组中的一个进程, 即进程不在前台, 于是我们要停止当前进程组的所有进程. 
+	// 因此这里就需要向当前进程组发送 SIGTTOU 信号, 并返回等待成为前台进程组后再执行写操作.
 	if (L_TOSTOP(tty) &&
 	    (current->tty == channel) && (tty->pgrp != current->pgrp))
 		return(tty_signal(SIGTTOU, tty));
-	// 现在我们开始从用户缓冲区 buf 中循环取出字符并放到写队列缓冲区中. 当欲写字节数大于 0, 则执行以下循环操作. 
-	// 在循环过程中, 如果此时 tty 写队列已满, 则当前进程进入可中断睡眠状态. 如果当前进程有信号要处理, 则退出循环体.
+	// 现在我们开始从用户缓冲区 buf 中循环取出字符并放到写队列缓冲区中. 
+	// 当欲写字节数大于 0, 则执行以下循环操作. 
+	// 在循环过程中, 如果此时 tty 写队列已满, 则当前进程进入可中断睡眠状态. 
+	// 如果当前进程有信号要处理, 则退出循环体.
 	while (nr > 0) {
 		sleep_if_full(tty->write_q);
 		if (current->signal & ~current->blocked)
@@ -533,13 +555,16 @@ int tty_write(unsigned channel, char * buf, int nr)
 				if (O_LCUC(tty))
 					c = toupper(c);									// 小写转成大写字符.
 			}
-			// 接着把用户数据缓冲指针 b 前移 1 字节; 欲写字节数减 1 字节; 复位 cr_flag 标志, 并将该字节放入 tty 写队列中.
+			// 接着把用户数据缓冲指针 b 前移 1 字节; 欲写字节数减 1 字节; 
+			// 复位 cr_flag 标志, 并将该字节放入 tty 写队列中.
 			b++; nr--;
 			cr_flag = 0;
 			PUTCH(c, tty->write_q);
 		}
-		// 若要求的字符全部写完, 或者写队列已满, 则程序退出循环. 此时会调用对应 tty 写函数, 把写队列缓冲区中的字符显示在控制台屏幕上, 或者通过串行端口发送出去. 
-		// 如果当前处理的 tty 是控制台终端, 那么 tty->write() 调用的是 con_write(); 如果 tty 是串行终端, 则 tty->write() 调用的是 rs_write() 函数. 
+		// 若要求的字符全部写完, 或者写队列已满, 则程序退出循环. 
+		// 此时会调用对应 tty 写函数, 把写队列缓冲区中的字符显示在控制台屏幕上, 或者通过串行端口发送出去. 
+		// 如果当前处理的 tty 是控制台终端, 那么 tty->write() 调用的是 con_write(); 
+		// 如果 tty 是串行终端, 则 tty->write() 调用的是 rs_write() 函数. 
 		// 若还有字节要写, 则等待写队列中字符取走. 所以这里调用调度程序, 先去执行其他任务.
 		tty->write(tty);
 		if (nr > 0)
@@ -563,7 +588,8 @@ int tty_write(unsigned channel, char * buf, int nr)
  * totally innocent.
  */
 /*
- * 呵, 有时我真的是很喜欢 386. 该子程序被从一个中断处理程序中调用, 并且即使在中断处理程序中睡眠也应该绝对没有问题(我希望如此). 
+ * 呵, 有时我真的是很喜欢 386. 
+ * 该子程序被从一个中断处理程序中调用, 并且即使在中断处理程序中睡眠也应该绝对没有问题(我希望如此). 
  * 当然, 如果有人证明我是错的, 那么我将憎恨 Intel 一辈子.
  *
  * 我不认为在通常环境会处在这里睡眠, 这样很好, 因为任务睡眠是完全任意的.
@@ -592,7 +618,7 @@ void tty_init(void)
 	// 对于串行终端(rs_queues)的读/写缓冲队列, 将它们的 data 字段设置为串行端口基地址值. 
 	// 串口 1 是 0x3f8, 串口 2 是 0x2f8. 然后先初步设置所有终端的 tty 结构.
 	// 其中特殊字符数组 c_cc[] 设置的初值定义在 include/linux/tty.h 文件中.
-	for (i = 0 ; i < QUEUES ; i++) 								// QUEUES = 54
+	for (i = 0 ; i < QUEUES ; i++) 							// QUEUES = 54
 		tty_queues[i] = (struct tty_queue) {0, 0, 0, 0, ""};
 	rs_queues[0] = (struct tty_queue) {0x3f8, 0, 0, 0, ""};
 	rs_queues[1] = (struct tty_queue) {0x3f8, 0, 0, 0, ""};
@@ -601,9 +627,9 @@ void tty_init(void)
 	// 初步设置所有终端的 tty 结构体
 	for (i = 0 ; i < 256 ; i++) {
 		tty_table[i] =  (struct tty_struct) {
-		 	{0, 0, 0, 0, 0, INIT_C_CC}, 						// termios: 终端 io 属性和控制字符数据结构
-			0, 0, 0, 											// pgrp(所属进程组), session(会话号), stopped(结束标志), 
-			NULL, NULL, NULL, NULL  							// *write(写函数), read_q(读缓冲队列), write_q(写缓冲队列), secondary(辅助缓冲队列)
+		 	{0, 0, 0, 0, 0, INIT_C_CC}, 					// termios: 终端 io 属性和控制字符数据结构
+			0, 0, 0, 										// pgrp(所属进程组), session(会话号), stopped(结束标志), 
+			NULL, NULL, NULL, NULL  						// *write(写函数), read_q(读缓冲队列), write_q(写缓冲队列), secondary(辅助缓冲队列)
 		};
 	}
 	// 接着初始化控制台终端(console.c). 
