7e8089df755f308c248444e3d30720d4f19806cd
8-bitgeek <zengqiangzhao@outlook.com>
2024-04-11 14:58:16 +0800

    add miniCRT implements, but not finished

--- miniCRT/malloc.c
+++ miniCRT/malloc.c
@@ -1,5 +1,9 @@
 #include "minicrt.h"
 
+#ifndef NULL
+#define NULL 0
+#endif
+
 typedef struct _heap_header {
     enum {
         HEAP_BLOCK_FREE = 0xABABABAB,
@@ -12,12 +16,13 @@ typedef struct _heap_header {
 
 static heap_header * list_head = NULL;
 
+#define ADDR_ADD(a, o) (((char *) (a)) + o)
 #define HEADER_SIZE (sizeof(heap_header))
 
 void * malloc(unsigned size) {
     heap_header * header;
     if (size == 0) {
-        return NULL:
+        return NULL;
     }
 
     header = list_head;
@@ -47,9 +52,35 @@ static int brk(void * end_data_segment) {
         "movl %1, %%ebx     \n\t"
         "int $0x80          \n\t"
         "movl %%eax, %0     \n\t"
-        : "=r"(ret)                             // r 表示通用寄存器.
-        : "m"(end_data_segment));               // m 表示内存变量.
+        : "=r"(ret)                             // '=' 表示操作数是只写的, 'r' 表示通用寄存器.
+        : "m"(end_data_segment));               // 'm' 表示内存变量.
+    return ret;
 }
-#else           // If define WIN32
+#else           // If define WIN32.
 #include <Windows.h>
 #endif
+
+int mini_crt_heap_init() {
+    void * base = NULL;
+    heap_header * header = NULL;
+    // 32MB heap size.
+    unsigned heap_size = 1024 * 1024 * 32;
+
+#ifdef WIN32
+#else 
+    base = (void *) brk(0);
+    void * end = ADDR_ADD(base, heap_size);
+    end = (void *) brk(end);
+    if (!end) {
+        return 0;
+    }
+#endif
+    header = (heap_header *) base;
+    header->size = heap_size;
+    header->type = HEAP_BLOCK_FREE;
+    header->next = NULL;
+    header->prev = NULL;
+
+    list_head = header;
+    return 1;
+}
