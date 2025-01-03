23c5402914fc1dfa168a1bdc616dbfa18e0025bc
gldwolf <zengqiangzhao@outlook.com>
2025-01-03 09:58:07 +0800

    update comments

--- include/sys/wait.h
+++ include/sys/wait.h
@@ -6,9 +6,6 @@
 #define _LOW(v)		( (v) & 0377)           // 取低字节(8 进制表示). 
 #define _HIGH(v)	( ((v) >> 8) & 0377)    // 取高字节. 
 
-/* options for waitpid, WUNTRACED not supported */
-/* waitpid 的选项, 其中 WUNTRACED 未被支持 */
-// [注: 其实 0.12 内核已经支持 WUNTRACED 选项. 上面这条注释应该是以前内核版本遗留下来的.  ]
 // 以下常数符号是函数 waitpid(pit_t pid, long *stat_addr, int options) 中 options 使用的选项. 
 #define WNOHANG		1       // 如果没有状态也不要挂起, 并立刻返回. 
 #define WUNTRACED	2       // 报告停止执行的子进程状态. 
@@ -17,11 +14,10 @@
 #define WIFEXITED(s)	(!((s)&0xFF))           // 如果子进程正常退出, 则为真. 
 #define WIFSTOPPED(s)	(((s)&0xFF)==0x7F)      // 如果子进程正停止着, 则为真. 
 #define WEXITSTATUS(s)	(((s)>>8)&0xFF)         // 返回退出状态. 
-#define WTERMSIG(s)	((s)&0x7F)                  // 返回导致进程终止的信号值(信号量). 
+#define WTERMSIG(s)	    ((s)&0x7F)              // 返回导致进程终止的信号值(信号量). 
 #define WCOREDUMP(s)	((s)&0x80)              // 判断进程是否执行了内在映像转储(dumpcore). 
-#define WSTOPSIG(s)	(((s)>>8)&0xFF)             // 返回导致进程停止的信号值. 
-#define WIFSIGNALED(s)	(((unsigned int)(s)-1 & 0xFFFF) < 0xFF) // 如果由于未捕捉信号而导致子
-                                                // 进程退出则为真. 
+#define WSTOPSIG(s)	    (((s)>>8)&0xFF)         // 返回导致进程停止的信号值. 
+#define WIFSIGNALED(s)	(((unsigned int)(s)-1 & 0xFFFF) < 0xFF) // 如果由于未捕捉信号而导致子进程退出则为真. 
 
 // wait() 和 waitpid() 函数允许进程获取与其子进程之一的状态信息. 各种选项允许获取已经终止或停止的子进程状态信息. 
 // 如果存在两个或两个以上子进程的状态信息, 则报告的顺序是不指定的. wait() 将挂起当前进程, 直到其子进程之一退出(终止), 
